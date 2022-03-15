45c013c586056fc1cf951e70383fa9000949a994
prakritichauhanpuresoftware <67620689+prakritichauhanpuresoftware@users.noreply.github.com>
2022-03-15 12:30:16 +0530

    Update build-wheels.sh

--- build-wheels.sh
+++ build-wheels.sh
@@ -17,6 +17,15 @@ for PYBIN in /opt/python/cp3*/bin; do
   fi
   rm -rf build
   export PATH=${PYBIN}:$OLD_PATH
+  if [ 'uname -m' == "i686" ]; then
+    curl -fsS -o rust-1.59.0-i686-unknown-linux-gnu.tar.gz https://static.rust-lang.org/dist/rust-1.59.0-i686-unknown-linux-gnu.tar.gz
+    tar -xf rust-1.59.0-i686-unknown-linux-gnu.tar.gz && cd rust-1.59.0-i686-unknown-linux-gnu
+    ./install.sh && cd ../
+    curl -fsS -o openssl-1.1.1k.tar.gz https://ftp.openssl.org/source/openssl-1.1.1k.tar.gz --no-check-certificate
+    tar -xzf openssl-1.1.1k.tar.gz && cd openssl-1.1.1k
+    ./config --prefix=/usr --openssldir=/etc/ssl --libdir=lib no-shared zlib-dynamic
+    make && make install && cd ../
+  fi
   POETRY_HOME=${PYBIN} ${PYBIN}/python install-poetry.py --preview -y
   export PATH=${PYBIN}/bin:$PATH
   poetry build -vvv
