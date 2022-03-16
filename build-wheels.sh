b5e219681f9f0862ef38339700a221bae32ac6fc
prakritichauhanpuresoftware <67620689+prakritichauhanpuresoftware@users.noreply.github.com>
2022-03-16 14:16:02 +0530

    Update build-wheels.sh

--- build-wheels.sh
+++ build-wheels.sh
@@ -5,16 +5,21 @@ cd $(dirname $0)
 
 curl -fsS -o install-poetry.py https://raw.githubusercontent.com/sdispater/poetry/master/install-poetry.py
 if [ $(uname -m) == 'i686' ]; then
-    yum update
-    curl -fsS -o rust-1.59.0-i686-unknown-linux-gnu.tar.gz https://static.rust-lang.org/dist/rust-1.59.0-i686-unknown-linux-gnu.tar.gz
-    tar -xf rust-1.59.0-i686-unknown-linux-gnu.tar.gz && cd rust-1.59.0-i686-unknown-linux-gnu
-    ./install.sh && cd ../
-    curl -fsS -o openssl-1.1.1k.tar.gz https://ftp.openssl.org/source/openssl-1.1.1k.tar.gz
-    tar -xzf openssl-1.1.1k.tar.gz && cd openssl-1.1.1k
-    ./config --prefix=/usr --openssldir=/etc/ssl --libdir=lib no-shared zlib-dynamic
-    make && make install && cd ../
- fi
-#rm get-poetry.py
+  yum update
+  curl -fsS -o rust-1.59.0-i686-unknown-linux-gnu.tar.gz https://static.rust-lang.org/dist/rust-1.59.0-i686-unknown-linux-gnu.tar.gz
+  tar -xf rust-1.59.0-i686-unknown-linux-gnu.tar.gz && cd rust-1.59.0-i686-unknown-linux-gnu
+  ./install.sh && cd ../
+  curl -fsS -o openssl-1.1.1k.tar.gz https://ftp.openssl.org/source/openssl-1.1.1k.tar.gz
+  tar -xzf openssl-1.1.1k.tar.gz && cd openssl-1.1.1k
+  ./config --prefix=/usr --openssldir=/etc/ssl --libdir=lib no-shared zlib-dynamic
+  make && make install && cd ../
+  export PLAT=manylinux2014_i686
+elif [ $(uname -m) == 'aarch64' ]; then
+  export PLAT=manylinux2014_aarch64
+else
+  export PLAT=manylinux2014_x86_64
+fi
+
 export OLD_PATH=$PATH
 
 for PYBIN in /opt/python/cp3*/bin; do
@@ -33,7 +38,7 @@ done
 
 cd dist
 for whl in *.whl; do
-    auditwheel repair "$whl"
+    auditwheel repair --plat $PLAT "$whl"
     rm "$whl"
 done
 ls -l wheelhouse/
